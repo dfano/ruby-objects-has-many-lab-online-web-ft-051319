eb58d8d01dde0da34f43f7cc2db4ca8061000c07
poignant-minifier-0983 <davidfano@gmail.com>
2019-06-08 02:00:13 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,6 @@
 class Author
   attr_accessor :name
     @@all = []
-    @posts = []
 
 
   def initialize(name)
@@ -13,11 +12,23 @@ class Author
     @@all
   end
 
-  def posts
-    @posts
-  end
-
   def add_post(post)
     post.author = self
   end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+
+  def self.post_count
+    Post.all.count
+  end
+
+
+
 end
